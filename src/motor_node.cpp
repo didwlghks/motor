548834e4cc42fd923dc446a5bc6fff99bda48e01
didwlghks <66863060+didwlghks@users.noreply.github.com>
2021-11-15 13:25:41 +0900

    Update motor_node.cpp

--- src/motor_node.cpp
+++ src/motor_node.cpp
@@ -400,27 +400,3 @@ void Motor_View()
 	printf("Acc  :%10.0d\n", acceleration);
 	printf("\n");
 }
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "motor_node");
-  ros::NodeHandle nh;
-  Initialize();
- ros::Rate loop_rate(Control_cycle);
-
-  while(ros::ok())
-  {
-    //Motor_Controller(1, true, 100);
-    //Motor_Controller(2, true, 100);
-    //Accel_Controller(1, true, 100);
-    //Accel_Controller(2, true, 100);
-    //Switch_Turn_Example(100, 100);
-    //Theta_Distance(180,100,30,110);
-    Motor_View();
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
-  Motor_Controller(1, true, 0);
-  Motor_Controller(2, true, 0);
-  return 0;
-}
