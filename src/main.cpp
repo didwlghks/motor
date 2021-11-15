c02cb7ae5a1158d7b57284ff0753d4459e04cc0a
didwlghks <66863060+didwlghks@users.noreply.github.com>
2021-11-15 13:32:45 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <motor_test/pid.h>
 #include <stdio.h>
 
+
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "motor_node");
@@ -27,3 +29,19 @@ int main(int argc, char** argv)
   Motor_Controller(2, true, 0);
   return 0;
 }
+
+/* //PID EXAMPLE
+int main() {
+
+    PID pid = PID(0.1, 100, -100, 0.1, 0.01, 0.5);
+
+    double val = 20;
+    for (int i = 0; i < 100; i++) {
+        double inc = pid.calculate(0, val);
+        printf("val:% 7.3f inc:% 7.3f\n", val, inc);
+        val += inc;
+    }
+
+    return 0;
+}
+*/
